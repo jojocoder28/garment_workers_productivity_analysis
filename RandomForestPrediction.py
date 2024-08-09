a0cc03012f8fedc2ffeec064c18146c3b1a25a09
jojocoder28 <dasjojo7@gmail.com>
2024-08-09 11:04:52 +0530

    fix bugs

--- RandomForestPrediction.py
+++ RandomForestPrediction.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 df=pd.read_csv("date_proper.csv")
 median_wip = df['wip'].median()
 df['wip'].fillna(median_wip, inplace=True)
